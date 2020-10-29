dc4587d4a305bf54e2b5413a23a8c04de174f77d
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2020-10-29 13:47:57 -0500

    Operadores JavaScript

---
+++ coertion.js
@@ -0,0 +1,5 @@
+const display = (toDisplay) => {
+    console.log(toDisplay);
+}
+
+display(true + false + true);
--- operators.js
+++ operators.js
@@ -9,3 +9,62 @@
 // a / b
 // 5. Operador de modulo %
 // a % b ; retorna el modulo de la división de a entre b
+/*
+6. Operador de incremento ++
+a++ aumenta en 1 el valor
+7. Operador de decremento --
+a-- reduce el valor en 1
+8. Operador de aumento y asignación
+a+=b; a=a+b
+9. Operador de reducción y asignación
+a-=b; a=a-b
+10. Operador de multiplicación y asignación
+a*=b; a=a*b
+11. Operador de división y asignación
+a/=; a=a/b
+*/
+
+// Operadores booleanos
+// 1. Operador AND &&
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+// 2. Operador OR ||
+/* 
+true || true = true
+true || false = true
+false || true = true
+false || false = false
+*/
+// 3. Operador NOT !
+/*
+!true = false
+!false = true
+*/
+
+// Operadores de comparación
+/*
+1. Operador mayor qué >
+a > b = true o false
+2. Operador menor qué <
+a < b = true o false
+3. Operador mayor o igual que >=
+a >= b = true o false
+4. Menor o igual que <=
+a <= b = true o false
+5. Operador de igualdad ==
+a == b = true o false
+6. Operador de igualdad estricta ===
+a === b = true o false
+7. Operador de diferencia !=
+a != b = true o false
+8. Operador de diferencia estricta !==
+a !== b = true o false
+9. Operador ternario ? :
+(expresión booleana) ? a : b
+10. Operador de nulidad ?.
+objeto?.valor
+11. Operador de asignación nula ??
+(null) ?? x
+*/
