39486345ddffee1be4c73931f0cbed6d41de1210
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2020-10-29 12:23:00 -0500

    Tipos de datos de JavaScript y declaración de variables

---
+++ index.js
@@ -0,0 +1,50 @@
+// TIPOS DE COMENTARIOS
+// ----------------------------------------------------
+// Esto es un comentario de linea
+/*
+Esto es un comentario de bloque
+*/
+
+// TIPOS DE DECLARACIÓN DE VARIABLES 
+// ----------------------------------------------------
+// Declaración de variable con var
+// var myNameWithVar;
+// Asignación de variable en linea independiente
+// myNameWithVar = 'Socar con var';
+
+// Declarion de variable con let
+// Declaración y asignación de una variable en una misma linea
+let myNameWithLet = 'Socar con let';
+
+// Declaración de una constante con const
+const myNameWithConst = 'Socar con const';
+
+// Declaración de una constante a la antigua
+// var MYNAMEWITHCONSTANT = 'Socar constante a la antigua';
+
+
+// TIPOS DE DATOS
+// ----------------------------------------------------
+// Tipo string o cadena de texto
+let stringTypeWithDoubleQuotes = "String con dobles comillas";
+let stringTypeWithSingleQuotes = 'String con comillas sencillas';
+
+// Tipo number o tipo númerico
+// VALOR MAXIMO = 1.7976931348623157e+308
+// VALOR MINIMO = 5e-324
+let typeNumber = 0;
+typeNumber = 1;
+typeNumber = 2;
+typeNumber = -3;
+// Tipo boolean/bool o tipo booleano
+let typeBooleanTrue = true;
+let typeBooleanFalse = false;
+
+// null
+// undefined
+// Para mas referencia, referirse a la analogia del papel higienico
+
+// VISUALIZACIÓN DE DATOS
+// ----------------------------------------------------
+// Imprimir cosas en consola
+console.log(myNameWithLet);
