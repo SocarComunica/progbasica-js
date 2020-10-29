b09de57a940f9ad38c5521264e1f48abdd49066d
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2020-10-29 13:06:37 -0500

    Declaración de funciones básicas y operadores aritmeticos básicos

---
+++ functions.js
@@ -0,0 +1,19 @@
+// Declaración de la función - función declarativa
+// function sayMyName() {
+//     console.log('Socar');
+// }
+
+// Llamado/invocación/ejecución de la función
+// sayMyName();
+
+// Crear un función que sume dos numeros cualquiera
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// Declaración de función expresiva - arrow function
+const sum = (a, b) => {
+    return a + b;
+}
+
+console.log(sum(1, 20));
---
+++ operators.js
@@ -0,0 +1,11 @@
+// Operadores aritmeticos
+// 1. Operador de suma +
+// a + b
+// 2. Operador de resta -
+// a - b
+// 3. Operador de multiplicación *
+// a * b
+// 4. Operador de división /
+// a / b
+// 5. Operador de modulo %
+// a % b ; retorna el modulo de la división de a entre b
