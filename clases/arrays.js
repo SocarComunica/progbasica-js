d399e94c2abf2d93f5e4c8ea4034a279fa6af566
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2020-11-03 14:11:27 -0500

    Ciclos for y arreglos

---
+++ clases/arrays.js
@@ -0,0 +1,11 @@
+/**
+ * Un array es una colección de datos o variables
+ * La posición inicial de un Array es la #0
+ */
+
+const notas = [];
+const notas2 = [1, 2, 3];
+const notas3 = [0, undefined, 1];
+notas3.push(1);
+console.log(notas3);
+
---
+++ clases/for.js
@@ -0,0 +1,5 @@
+const patoEsGay = 'Pato es gay';
+
+for (let i = 0; i < 4; i--) {
+    console.log(patoEsGay);
+}
