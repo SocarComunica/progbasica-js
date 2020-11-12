f45a03b5026e8c36c5356ef8294baade66dfa98d
Nicolas Patarroyo <nicolaspatarroyo@hotmail.com>
2020-11-12 11:57:23 -0500

    Ciclos while y do-while

---
+++ clases/do-while.js
@@ -0,0 +1,5 @@
+let random;
+do {
+    random = Math.floor(Math.random() * 10);
+    console.log(random);
+} while (random != 5)
---
+++ clases/while.js
@@ -0,0 +1,27 @@
+let control = 1;
+while (control >= 3) {
+    console.log('░░░░░░░░░░░░░░░░░░░░░░░░░░░');
+    console.log('░█▀▀█░░█▀▀▀░█▀▀█░█▀▀▀░█▀▀█░');
+    console.log('░█▀▀█░░█░░░░█▀▀█░▀▀▀█░█▀▀█░');
+    console.log('░▀░░▀░░▀▀▀▀░▀░░▀░▀▀▀▀░▀░░▀░');
+    console.log('░░█▀▀█░█▀▀░▀▀█▀▀░█▀▀░█▀▀░░░');
+    console.log('░░█▀▀▀░█▀▀░░░█░░░█▀▀░▀▀█░░░');
+    console.log('░░▀░░░░▀▀▀░░░▀░░░▀▀▀░▀▀░░');
+    if (control === 3) {
+        console.log('═══•◉•═════');
+        console.log('▂▄▄▓▄▄▂');
+        console.log('◢◤ █▀▀████▄▄▄▄◢◤');
+        console.log('█▄ █ █▄ ███▀▀▀▀▀▀▀╬');
+        console.log('◥█████◤');
+        console.log('═╩══╩═');
+        console.log('╬═╬');
+        console.log('╬═╬');
+        console.log('╬═╬');
+        console.log('╬═╬ Besitos en la Cola GG Ez');
+        console.log('╬═╬  ●/');
+        console.log('╬═╬ /▌');
+        console.log('╬═╬ / \\');
+    }
+    control++;
+}
+console.log('Bye, besitos en la cola');
